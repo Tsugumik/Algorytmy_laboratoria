a0a15d33e71f8f3305d0f63a37e608f63b3313cf
Błażej Drozd <drozdbazej@gmail.com>
2023-10-21 13:54:57 +0200

    Moved docs comments from cpp files to header files.

--- algorithms/Algorithms.h
+++ algorithms/Algorithms.h
@@ -5,15 +5,95 @@ int NWW(int, int);
 int Sum_of_digits(int);
 
 namespace Array_tools {
+    /**
+     * Funkcja znajduje maksymalną wartość w tablicy.
+     * @param arr - Tablica liczb całkowitych.
+     * @param size - Rozmiar tablicy.
+     * @return Maksymalna wartość w tablicy.
+     */
     int Max(const int arr[], int size);
+
+    /**
+     * Funkcja znajduje minimalną wartość w tablicy.
+     * @param arr - Tablica liczb całkowitych.
+     * @param size - Rozmiar tablicy.
+     * @return Minimalna wartość w tablicy.
+     */
     int Min(const int arr[], int size);
+
+    /**
+     * Funkcja oblicza rozrzut wartości w tablicy.
+     * @param arr - Tablica liczb całkowitych.
+     * @param size - Rozmiar tablicy.
+     * @return Rozrzut wartości w tablicy.
+     */
     int Val_dispersion(const int arr[], int size);
+
+    /**
+     * Funkcja oblicza średnią elementów w tablicy.
+     * Przeciążenie dla tablicy liczb całkowitych.
+     * @param arr - Tablica liczb całkowitych.
+     * @param size - Rozmiar tablicy.
+     * @return Średnia arytmetyczna elementów w tablicy.
+     */
     double Avg(const int arr[], int size);
+
+    /**
+     * Funkcja oblicza średnią elementów w tablicy.
+     * Przeciążenie dla tablicy liczb zmiennoprzecinkowych.
+     * @param arr - Tablica liczb zmiennoprzecinkowych.
+     * @param size - Rozmiar tablicy.
+     * @return Średnia arytmetyczna elementów w tablicy.
+     */
     double Avg(const double arr[], int size);
+
+    /**
+     * Funkcja iteracyjnie oblicza sumę wszystkich elementów w tablicy.
+     * Przeciążenie dla tablicy liczb całkowitych.
+     * @param arr - Tablica liczb całkowitych.
+     * @param size - Rozmiar tablicy.
+     * @return Suma wszystkich elementów w tablicy.
+     */
     int Sum(const int arr[], int size);
+
+    /**
+     * Funkcja iteracyjnie oblicza sumę wszystkich elementów w tablicy.
+     * Przeciążenie dla tablicy liczb zmiennoprzecinkowych.
+     * @param arr - Tablica liczb zmiennoprzecinkowych.
+     * @param size - Rozmiar tablicy.
+     * @return Suma wszystkich elementów w tablicy.
+     */
     double Sum(const double arr[], int size);
+
+    /**
+     * Funkcja oblicza odchylenie standardowe.
+     * @param arr - Tablica liczb całkowitych.
+     * @param size - Rozmiar tablicy.
+     * @return Odchylenie standardowe tablicy.
+     */
     double Standard_deviation(const int arr[], int size);
+
+    /**
+     * Funkcja oblicza wariancję.
+     * @param arr - Tablica liczb całkowitych.
+     * @param size - Rozmiar tablicy.
+     * @return Wariancja tablicy.
+     */
     double Variance(const int arr[], int size);
+
+    /**
+     * Funkcja zwraca n-ty element tablicy.
+     * @param arr - Tablica liczb całkowitych.
+     * @param n - Numer elementu, który ma zostać zwrócony.
+     * @return N-ty element tablicy.
+     */
     int Get_n_element(const int arr[], int n);
+
+    /**
+     * Funkcja zwraca ilość elementów w tablicy podzielnych przez 3.
+     * @param arr - Tablica liczb całkowitych.
+     * @param size - Rozmiar tablicy.
+     * @return Ilość elementów podzielnych przez 3 w tablicy.
+     */
     int Count_divisible_by_3(const int arr[], int size);
 }
--- algorithms/Array_tools.cpp
+++ algorithms/Array_tools.cpp
@@ -5,12 +5,6 @@
 using namespace Math_functions;
 
 namespace Array_tools {
-    /**
-     * Funkcja znajduje maksymalną wartość w tablicy.
-     * @param arr - Tablica liczb całkowitych.
-     * @param size - Rozmiar tablicy.
-     * @return Maksymalna wartość w tablicy.
-     */
     int Max(const int arr[], const int size) {
 
         int max = arr[0];
@@ -20,12 +14,6 @@ namespace Array_tools {
         return max;
     }
 
-    /**
-     * Funkcja znajduje minimalną wartość w tablicy.
-     * @param arr - Tablica liczb całkowitych.
-     * @param size - Rozmiar tablicy.
-     * @return Minimalna wartość w tablicy.
-     */
     int Min(const int arr[], int size) {
         int min = arr[0];
         for(int i=1; i<size; i++) {
@@ -34,47 +22,20 @@ namespace Array_tools {
         return min;
     }
 
-    /**
-     * Funkcja oblicza rozrzut wartości w tablicy.
-     * @param arr - Tablica liczb całkowitych.
-     * @param size - Rozmiar tablicy.
-     * @return Rozrzut wartości w tablicy.
-     */
     int Val_dispersion(const int arr[], int size) {
         return Max(arr, size) - Min(arr, size);
     }
 
-    /**
-     * Funkcja oblicza średnią elementów w tablicy.
-     * Przeciążenie dla tablicy liczb całkowitych.
-     * @param arr - Tablica liczb całkowitych.
-     * @param size - Rozmiar tablicy.
-     * @return Średnia arytmetyczna elementów w tablicy.
-     */
     double Avg(const int arr[], int size) {
         int sum = Sum(arr, size);
         return (double)sum/size;
     }
 
-    /**
-     * Funkcja oblicza średnią elementów w tablicy.
-     * Przeciążenie dla tablicy liczb zmiennoprzecinkowych.
-     * @param arr - Tablica liczb zmiennoprzecinkowych.
-     * @param size - Rozmiar tablicy.
-     * @return Średnia arytmetyczna elementów w tablicy.
-     */
     double Avg(const double arr[], int size) {
         double sum = Sum(arr, size);
         return sum/size;
     }
 
-    /**
-     * Funkcja iteracyjnie oblicza sumę wszystkich elementów w tablicy.
-     * Przeciążenie dla tablicy liczb całkowitych.
-     * @param arr - Tablica liczb całkowitych.
-     * @param size - Rozmiar tablicy.
-     * @return Suma wszystkich elementów w tablicy.
-     */
     int Sum(const int arr[], int size) {
         int sum = 0;
         for(int i=0; i<size; i++) {
@@ -83,13 +44,6 @@ namespace Array_tools {
         return sum;
     }
 
-    /**
-     * Funkcja iteracyjnie oblicza sumę wszystkich elementów w tablicy.
-     * Przeciążenie dla tablicy liczb zmiennoprzecinkowych.
-     * @param arr - Tablica liczb zmiennoprzecinkowych.
-     * @param size - Rozmiar tablicy.
-     * @return Suma wszystkich elementów w tablicy.
-     */
     double Sum(const double arr[], int size) {
         double sum = 0.0;
         for(int i=0; i<size; i++) {
@@ -98,22 +52,11 @@ namespace Array_tools {
         return sum;
     }
 
-    /**
-     * Funkcja oblicza odchylenie standardowe.
-     * @param arr - Tablica liczb całkowitych.
-     * @param size - Rozmiar tablicy.
-     * @return Odchylenie standardowe tablicy.
-     */
+
     double Standard_deviation(const int arr[], int size) {
         return sqrt(Variance(arr, size));
     }
 
-    /**
-     * Funkcja oblicza wariancję.
-     * @param arr - Tablica liczb całkowitych.
-     * @param size - Rozmiar tablicy.
-     * @return Wariancja tablicy.
-     */
     double Variance(const int arr[], int size) {
         double diff_squares[size];
 
@@ -132,22 +75,10 @@ namespace Array_tools {
 
     }
 
-    /**
-     * Funkcja zwraca n-ty element tablicy.
-     * @param arr - Tablica liczb całkowitych.
-     * @param n - Numer elementu, który ma zostać zwrócony.
-     * @return N-ty element tablicy.
-     */
     int Get_n_element(const int arr[], int n) {
         return arr[n];
     }
 
-    /**
-     * Funkcja zwraca ilość elementów w tablicy podzielnych przez 3.
-     * @param arr - Tablica liczb całkowitych.
-     * @param size - Rozmiar tablicy.
-     * @return Ilość elementów podzielnych przez 3 w tablicy.
-     */
     int Count_divisible_by_3(const int arr[], int size) {
         int count = 0;
 
