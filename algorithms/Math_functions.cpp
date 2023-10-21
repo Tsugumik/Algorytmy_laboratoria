b8ad71b17bf74eef08d3c1ac67dff91b213f53a5
Błażej Drozd <drozdbazej@gmail.com>
2023-10-21 14:38:12 +0200

    Moved docs comments from cpp files to header files.

--- algorithms/Math_functions.cpp
+++ algorithms/Math_functions.cpp
@@ -2,11 +2,6 @@
 #include <iostream>
 
 namespace Math_functions {
-    /**
-     * Funkcja oblicza silnię liczby całkowitej.
-     * @param n - Liczba całkowita, dla której obliczana jest silnia.
-     * @return Silnia liczby n.
-     */
     int Factorial(int n) {
         int factorial = 1;
 
@@ -17,11 +12,6 @@ namespace Math_functions {
         return factorial;
     }
 
-    /**
-     * Funkcja rekurencyjnie oblicza silnię liczby całkowitej.
-     * @param n - Liczba całkowita, dla której obliczana jest silnia.
-     * @return Silnia liczby n.
-     */
     int Factorial_rec(int n) {
         if (n < 2) {
             return 1;
@@ -30,12 +20,6 @@ namespace Math_functions {
         }
     }
 
-    /**
-     * Funkcja oblicza a do potęgi n.
-     * @param a - Podstawa potęgi.
-     * @param n - Wykładnik potęgi.
-     * @return Wynik potęgowania a do potęgi n.
-     */
     int POW(int a, int n) {
         if (n == 0) {
             return 1;
@@ -50,13 +34,6 @@ namespace Math_functions {
         return pow;
     }
 
-    /**
-     * Funkcja oblicza a do potęgi n
-     * Przeciążenie dla liczb podstawy zmiennoprzecinkowej.
-     * @param a - Podstawa potęgi.
-     * @param n - Wykładnik potęgi.
-     * @return Wynik potęgowania a do potęgi n.
-     */
     double POW(double a, int n) {
         if (n == 0) {
             return 1;
@@ -71,24 +48,10 @@ namespace Math_functions {
         return pow;
     }
 
-    /**
-     * Funkcja oblicza deltę dla równania kwadratowego ax^2 + bx + c.
-     * @param a - Współczynnik a.
-     * @param b - Współczynnik b.
-     * @param c - Współczynnik c.
-     * @return Delta równania kwadratowego.
-     */
     double Delta(const double &a, const double &b, const double &c) {
         return POW(b, 2) - (4 * a * c);
     }
 
-    /**
-     * Funkcja rozwiązuje równanie kwadratowe ax^2 + bx + c.
-     * Wyświetla pierwiastki równania lub informację o ich braku.
-     * @param a - Współczynnik a.
-     * @param b - Współczynnik b.
-     * @param c - Współczynnik c.
-     */
     void Quadratic_equation(const double &a, const double &b, const double &c) {
         using std::cout, std::endl;
 
@@ -114,12 +77,6 @@ namespace Math_functions {
 
     }
 
-    /**
-     * Funkcja rozwiązuje równanie liniowe ax + b.
-     * Wyświetla pierwiastek równania lub informację o braku rozwiązania.
-     * @param a - Współczynnik a.
-     * @param b - Współczynnik b.
-     */
     void Linear_equation(const double &a, const double &b) {
         using std::cout, std::endl;
         double x;
--- algorithms/Math_functions.h
+++ algorithms/Math_functions.h
@@ -1,17 +1,60 @@
 #pragma once
 
 namespace Math_functions {
+    /**
+     * Funkcja oblicza silnię liczby całkowitej.
+     * @param n - Liczba całkowita, dla której obliczana jest silnia.
+     * @return Silnia liczby n.
+     */
     int Factorial(int);
 
+    /**
+     * Funkcja rekurencyjnie oblicza silnię liczby całkowitej.
+     * @param n - Liczba całkowita, dla której obliczana jest silnia.
+     * @return Silnia liczby n.
+     */
     int Factorial_rec(int);
 
+    /**
+     * Funkcja oblicza a do potęgi n.
+     * @param a - Podstawa potęgi.
+     * @param n - Wykładnik potęgi.
+     * @return Wynik potęgowania a do potęgi n.
+     */
     int POW(int, int);
 
+    /**
+     * Funkcja oblicza a do potęgi n
+     * Przeciążenie dla liczb podstawy zmiennoprzecinkowej.
+     * @param a - Podstawa potęgi.
+     * @param n - Wykładnik potęgi.
+     * @return Wynik potęgowania a do potęgi n.
+     */
     double POW(double, int);
 
+    /**
+     * Funkcja oblicza deltę dla równania kwadratowego ax^2 + bx + c.
+     * @param a - Współczynnik a.
+     * @param b - Współczynnik b.
+     * @param c - Współczynnik c.
+     * @return Delta równania kwadratowego.
+     */
     double Delta(const double &, const double &, const double &);
 
+    /**
+     * Funkcja rozwiązuje równanie kwadratowe ax^2 + bx + c.
+     * Wyświetla pierwiastki równania lub informację o ich braku.
+     * @param a - Współczynnik a.
+     * @param b - Współczynnik b.
+     * @param c - Współczynnik c.
+     */
     void Quadratic_equation(const double &, const double &, const double &);
 
+    /**
+     * Funkcja rozwiązuje równanie liniowe ax + b.
+     * Wyświetla pierwiastek równania lub informację o braku rozwiązania.
+     * @param a - Współczynnik a.
+     * @param b - Współczynnik b.
+     */
     void Linear_equation(const double &, const double &);
 }
