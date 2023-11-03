eaadd8e3b579097f7ddb4e64fa428b4aa7bccdb9
Błażej Drozd <drozdbazej@gmail.com>
2023-11-03 11:55:22 +0100

    Deleted typo

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
     /*
      * Instrukcja:
      * Aby wykonać dane zadanie należy uruchomić je z danej przestrzeni nazw
-     * zadania_DATAZAJĘĆ::zad_NUMBERZADANIA
+     * zadania_DATAZAJĘĆ::zad_NUMERZADANIA
      * np. zadania_25_10_2023::zad_1();
      */
     return 0;
