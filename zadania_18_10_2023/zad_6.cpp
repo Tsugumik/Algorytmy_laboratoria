a212e60bb650ba4803102dd61f54eba3d9407449
Błażej Drozd <drozdbazej@gmail.com>
2023-11-11 21:01:27 +0100

    Modified exercise 6. from LAB2.

--- zadania_18_10_2023/zad_6.cpp
+++ zadania_18_10_2023/zad_6.cpp
@@ -6,20 +6,35 @@ namespace zadania_18_10_2023 {
         using std::cout, std::cin, std::endl;
         using namespace Array_tools;
 
-        const int size = 10;
+        int size;
 
-        int arr[10]{
-                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-        };
+        cout << "Podaj rozmiar tablicy:";
+        cin >> size;
 
-        cout << Max(arr, size) << endl;
-        cout << Min(arr, size) << endl;
-        cout << Val_dispersion(arr, size) << endl;
-        cout << Avg(arr, size) << endl;
-        cout << Sum(arr, size) << endl;
-        cout << Standard_deviation(arr, size) << endl;
-        cout << Variance(arr, size) << endl;
-        cout << Count_divisible_by_3(arr, size) << endl;
-        cout << Get_n_element(arr, 1) << endl;
+        int* arr = new int[size];
+
+        cout << "Przyklad: 7 8 5 5" << endl;
+        cout << "Wpisz wszystkie elementy tablicy oddzielone spacjami i zatwierdz enterem:";
+
+        for(int i=0; i<size; i++) {
+            cin >> arr[i];
+        }
+
+        cout << "Najwiekszy element: " << Max(arr, size) << endl;
+        cout << "Najmniejszy element: " << Min(arr, size) << endl;
+        cout << "Rozrzut wartosci: " << Val_dispersion(arr, size) << endl;
+        cout << "Srednia arytmetyczna: " << Avg(arr, size) << endl;
+        cout << "Suma wartosci: " << Sum(arr, size) << endl;
+        cout << "Odchylenie standardowe: " << Standard_deviation(arr, size) << endl;
+        cout << "Wariancja: " << Variance(arr, size) << endl;
+        cout << "Ilosc elementow podzielnych przez 3: " << Count_divisible_by_3(arr, size) << endl;
+
+        int n;
+        cout << "Podaj ktory n-ty element tablicy chcesz wypisac: ";
+        cin >> n;
+
+        cout << n << ". element tablicy: " << Get_n_element(arr, 0) << endl;
+
+        delete[] arr;
     }
 }
