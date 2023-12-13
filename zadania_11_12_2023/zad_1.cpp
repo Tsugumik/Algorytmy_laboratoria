7e6295a0ca346164a3c7ad4667deef06516be520
Błażej Drozd <drozdbazej@gmail.com>
2023-12-13 14:35:25 +0100

    Delete ; after include

--- zadania_11_12_2023/zad_1.cpp
+++ zadania_11_12_2023/zad_1.cpp
@@ -1,5 +1,5 @@
 #include "../zadania.h"
-#include "../time_measure/Gen_time_test_data.h";
+#include "../time_measure/Gen_time_test_data.h"
 
 void zadania_11_12_2023::zad_1() {
     Gen_time_test_data("./data");
